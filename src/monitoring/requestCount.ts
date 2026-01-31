d1649e700f30b5b55c3ac2ba3ed3bc9701dab420
Mrinal Sharma <mrinalwork2@gmail.com>
2026-01-31 18:01:48 -0500

    Revert "Prometheus init config"
    This reverts commit d4b10e6ebc22cfd52741c14026dc0c5d75d0dca4.

--- src/monitoring/requestCount.ts
+++
@@ -1,8 +0,0 @@
-import client from "prom-client";
-
-export const requestCount = new client.Counter({
-    name: "http_request_count",
-    help: "Total number of HTTP requests",
-    labelNames: ["method", "route", "status_code"],
-})
-
